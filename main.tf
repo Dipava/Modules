e11031bf70e7b7b160db888aa7a0e9075ad2c1ed
Dipava <dipavo.roy@gmail.com>
2022-12-22 02:25:46 +0530

    egress ingress

--- main.tf
+++ main.tf
@@ -37,6 +37,14 @@ module "vpc" {
   subnet_cidr  = var.vpc_subnets
   enable_dns_hostnames = false
   enable_dns_support   = true
+  ingress_source_port = 
+  ingress_destination_port = 
+  ingress_protocol = 
+  ingress_cidr = 
+  egress_source_port = 
+  egress_destination_port = 
+  egress_protocol = 
+  egress_cidr = 
 }
 
 resource "aws_instance" "test-vm" {
