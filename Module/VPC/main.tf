60ffeebb62ea1126ba1bb4955b4fc8a0d5dcdf20
Dipava <dipavo.roy@gmail.com>
2022-12-22 15:51:22 +0530

    route update

--- Module/VPC/main.tf
+++ Module/VPC/main.tf
@@ -42,7 +42,7 @@ resource "aws_route_table" "vpc-rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = var.cidr
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
